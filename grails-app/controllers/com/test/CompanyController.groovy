f87f23a7dee3451b768961cee944dedba9ba4a3b
AndrewEvl <Lino220037@gmail.com>
2018-08-07 17:45:44 +0300

    50\50 work parsing

--- grails-app/controllers/com/test/CompanyController.groovy
+++ grails-app/controllers/com/test/CompanyController.groovy
@@ -1,8 +1,11 @@
 package com.test
 
 import grails.validation.ValidationException
+import groovy.sql.Sql
+
 import static org.springframework.http.HttpStatus.*
 import com.xlson.groovycsv.CsvParser
+import com.opencsv.CSVReader
 
 class CompanyController {
 
@@ -12,28 +15,28 @@ class CompanyController {
 
     def index(Integer max) {
         params.max = Math.min(max ?: 10, 100)
-        respond companyService.list(params), model:[companyCount: companyService.count()]
+        respond companyService.list(params), model: [companyCount: companyService.count()]
     }
 
     def show(Long id) {
         [company: companyService.get(id)]
     }
 
-    def findByName (String name){
+    def findByName(String name) {
         def company = Company.findByName(name)
         if (!company.getName().isEmpty()) {
             render(view: 'show', model: [company: companyService.get(company.getId())])
         } else redirect(action: "index")
     }
 
-    def findByStreet (String street){
+    def findByStreet(String street) {
         def company = Company.findByStreet(street)
         if (!company.getName().isEmpty()) {
             render(view: 'show', model: [company: companyService.get(company.getId())])
         } else redirect(action: "index")
     }
 
-    def findByEmail (String email){
+    def findByEmail(String email) {
         def company = Company.findByEmail(email)
         if (!company.getName().isEmpty()) {
             render(view: 'show', model: [company: companyService.get(company.getId())])
@@ -41,102 +44,138 @@ class CompanyController {
     }
 
     def upload = {
-//        def company = new Company()
-//        new File("users.csv").splitEachLine(",") { fields ->
-//            company.add(
-//                    name: fields[0],
+//        request.getFile('filecsv')
+//                .inputStream
+//                .splitEachLine(',') { fields ->
+//            def company = new Company(name: fields[0].trim(),
+//                    email: fields[1].trim(),
+//                    street: fields[2].trim(),
+//                    zip: fields[3].trim())
+//
+//            if (company.hasErrors() || company.save(flush: true) == null) {
+//                log.error("Could not import domainObject  ${company.errors}")
+//            }
+//
+//            log.debug("Importing domainObject  ${company.toString()}")
+//        }
+
+//        def sql = Sql.newInstance("jdbc:mysql://localhost:3306/demo", "root", "root", "com.mysql.cj.jdbc.Driver")
+//        def company = sql.dataSet("company")
+//        new File("C:\\Users\\ami\\Desktop\\1.csv").splitEachLine(",") { fields ->
+//            company.add(name: fields[0],
 //                    email: fields[1],
-//                    street: fields[2])
+//                    street: fields[2],
+//                    zip: fields[3]
+//            )
 
-        println params.filecsv
-        def data = CsvParser.parseCsv('filecsv')
-        for (line in data) {
-            def company = new Company(
-                    name: "${line.name}",
-                    street: "${line.street}",
-                    email: "${line.email}",
-                    zip: "${line.zip}"
-            )
-            company.save()
-        }
-    }
+        new File("C:\\Users\\ami\\Desktop\\1.csv").splitEachLine(',') { fields ->
+            def company = new Company(name: fields[0].trim(),
+                    street: fields[1].trim(),
+                    email: fields[2].trim(),
+                    zip: fields[3].trim())
 
-    def create() {
-        respond new Company(params)
-    }
-
-    def save(Company company) {
-        if (company == null) {
-            notFound()
-            return
-        }
-
-        try {
-            companyService.save(company)
-        } catch (ValidationException e) {
-            respond company.errors, view:'create'
-            return
-        }
-
-        request.withFormat {
-            form multipartForm {
-                flash.message = message(code: 'default.created.message', args: [message(code: 'company.label', default: 'Company'), company.id])
-                redirect company
+            if (company.hasErrors() || company.save(flush: true) == null) {
+                log.error("___not save ${company.name}")
             }
-            '*' { respond company, [status: CREATED] }
+            log.debug("___Importing domainObject  ${company.toString()}")
+
         }
+//        println params.filecsv
+//        def data = CsvParser.parseCsv("C:\\Users\\ami\\Desktop\\1.csv")
+//        for (line in data) {
+//            def company = new Company(
+//                    name: "${line.name}",
+//                    street: "${line.street}",
+//                    email: "${line.email}",
+//                    zip: "${line.zip}"
+//            )
+//            company.save()
+//        }
+
+//        List<String[]> company = new CSVReader(new InputStreamReader(getClass()
+//                .classLoader
+//                .getResourceAsStream('filecsv')))
+//                .readAll()
+//        println company
     }
 
-    def edit(Long id) {
-        respond companyService.get(id)
+def create() {
+    respond new Company(params)
+}
+
+def save(Company company) {
+    if (company == null) {
+        notFound()
+        return
     }
 
-    def update(Company company) {
-        if (company == null) {
-            notFound()
-            return
-        }
-
-        try {
-            companyService.save(company)
-        } catch (ValidationException e) {
-            respond company.errors, view:'edit'
-            return
-        }
-
-        request.withFormat {
-            form multipartForm {
-                flash.message = message(code: 'default.updated.message', args: [message(code: 'company.label', default: 'Company'), company.id])
-                redirect company
-            }
-            '*'{ respond company, [status: OK] }
-        }
+    try {
+        companyService.save(company)
+    } catch (ValidationException e) {
+        respond company.errors, view: 'create'
+        return
     }
 
-    def delete(Long id) {
-        if (id == null) {
-            notFound()
-            return
+    request.withFormat {
+        form multipartForm {
+            flash.message = message(code: 'default.created.message', args: [message(code: 'company.label', default: 'Company'), company.id])
+            redirect company
         }
+        '*' { respond company, [status: CREATED] }
+    }
+}
 
-        companyService.delete(id)
+def edit(Long id) {
+    respond companyService.get(id)
+}
 
-        request.withFormat {
-            form multipartForm {
-                flash.message = message(code: 'default.deleted.message', args: [message(code: 'company.label', default: 'Company'), id])
-                redirect action:"index", method:"GET"
-            }
-            '*'{ render status: NO_CONTENT }
-        }
+def update(Company company) {
+    if (company == null) {
+        notFound()
+        return
     }
 
-    protected void notFound() {
-        request.withFormat {
-            form multipartForm {
-                flash.message = message(code: 'default.not.found.message', args: [message(code: 'company.label', default: 'Company'), params.id])
-                redirect action: "index", method: "GET"
-            }
-            '*'{ render status: NOT_FOUND }
-        }
+    try {
+        companyService.save(company)
+    } catch (ValidationException e) {
+        respond company.errors, view: 'edit'
+        return
     }
+
+    request.withFormat {
+        form multipartForm {
+            flash.message = message(code: 'default.updated.message', args: [message(code: 'company.label', default: 'Company'), company.id])
+            redirect company
+        }
+        '*' { respond company, [status: OK] }
+    }
+}
+
+def delete(Long id) {
+    if (id == null) {
+        notFound()
+        return
+    }
+
+    companyService.delete(id)
+
+    request.withFormat {
+        form multipartForm {
+            flash.message = message(code: 'default.deleted.message', args: [message(code: 'company.label', default: 'Company'), id])
+            redirect action: "index", method: "GET"
+        }
+        '*' { render status: NO_CONTENT }
+    }
+}
+
+protected void notFound() {
+    request.withFormat {
+        form multipartForm {
+            flash.message = message(code: 'default.not.found.message', args: [message(code: 'company.label', default: 'Company'), params.id])
+            redirect action: "index", method: "GET"
+        }
+        '*' { render status: NOT_FOUND }
+    }
+}
+
 }
